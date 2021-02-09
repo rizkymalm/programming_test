368f6ff64667e6fff125e9b63b44ee696d33b635
rizkymalm <rizkymalm@gmail.com>
2021-02-09 23:32:45 +0700

    edit

---
+++ rawdata.js
@@ -0,0 +1,252 @@
+function getData(){
+  var json = 
+  [
+  {
+    "Nama": "Riana Utami",
+    "Kota": "1",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "3"
+  },
+  {
+    "Nama": "Rahmat",
+    "Kota": "1",
+    "jenis_kelamin": "2",
+    "pendidikan": "5",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "Kartika Sari",
+    "Kota": "1",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "Murniati",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "Sri widayati",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "5"
+  },
+  {
+    "Nama": "Rusdi Maryadi",
+    "Kota": "3",
+    "jenis_kelamin": "1",
+    "pendidikan": "4",
+    "pekerjaan": "3"
+  },
+  {
+    "Nama": "Rudy",
+    "Kota": "3",
+    "jenis_kelamin": "1",
+    "pendidikan": "4",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "rahayu",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "5",
+    "pekerjaan": "3"
+  },
+  {
+    "Nama": "tes",
+    "Kota": "2",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "Murni susilowati",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "Toto",
+    "Kota": "3",
+    "jenis_kelamin": "1",
+    "pendidikan": "4",
+    "pekerjaan": "3"
+  },
+  {
+    "Nama": "risa",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "Tri Suhartatik",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "5"
+  },
+  {
+    "Nama": "elly",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "abel riswandi",
+    "Kota": "1",
+    "jenis_kelamin": "1",
+    "pendidikan": "4",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "Sunayah",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "nanik",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "6",
+    "pekerjaan": "5"
+  },
+  {
+    "Nama": "Suwarso",
+    "Kota": "2",
+    "jenis_kelamin": "1",
+    "pendidikan": "4",
+    "pekerjaan": "2"
+  },
+  {
+    "Nama": "Rachmadhani",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "6",
+    "pekerjaan": "3"
+  },
+  {
+    "Nama": "rohmania",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "2"
+  },
+  {
+    "Nama": "Marrya Septika",
+    "Kota": "1",
+    "jenis_kelamin": "2",
+    "pendidikan": "6",
+    "pekerjaan": "6"
+  },
+  {
+    "Nama": "teti haryati",
+    "Kota": "2",
+    "jenis_kelamin": "2",
+    "pendidikan": "6",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "Deddy",
+    "Kota": "3",
+    "jenis_kelamin": "1",
+    "pendidikan": "7",
+    "pekerjaan": "6"
+  },
+  {
+    "Nama": "Umi Alfiatul",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "yuli",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "Nancy",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "5",
+    "pekerjaan": "2"
+  },
+  {
+    "Nama": "Nanik",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "6"
+  },
+  {
+    "Nama": "Busiah",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "5"
+  },
+  {
+    "Nama": "Herva",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "Sri Indrawati",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "Yanti Dwi jayanti",
+    "Kota": "1",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "4"
+  },
+  {
+    "Nama": "maria",
+    "Kota": "1",
+    "jenis_kelamin": "2",
+    "pendidikan": "5",
+    "pekerjaan": "6"
+  },
+  {
+    "Nama": "umi",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "5",
+    "pekerjaan": "1"
+  },
+  {
+    "Nama": "anik",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "4",
+    "pekerjaan": "3"
+  },
+  {
+    "Nama": "ika Purwanti",
+    "Kota": "3",
+    "jenis_kelamin": "2",
+    "pendidikan": "6",
+    "pekerjaan": "1"
+  }
+ ];
+
+ return json;
+}
